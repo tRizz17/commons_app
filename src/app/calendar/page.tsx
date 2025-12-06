82a828409fdc1b711d7b58ebbb0cf8478b1d5000
Thomas Rizzo <rizzot@oregonstate.edu>
2025-12-06 11:22:45 -0800

    calendar page maps lists for each month, no info included yet

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,13 +1,25 @@
-'use client'
-import ProfileHeader from "@/components/loggedInNav"
+
+
+
+const items = [{ id: 1 }, { id: 2 }, { id: 3 }]
+const months = [{ id: 1 }, { id: 2 }, { id: 3 }]
+
 
 export default function Calendar() {
-
     return (
-        <div>
-            {/* <ProfileHeader></ProfileHeader> */}
-            <div>Calendar</div>
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 border h-screen">
+            {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
+            {months.map((month) => (
+                <div key={month.id} className="mx-auto max-w-3xl border mt-6">
+                    <ul role="list" className="space-y-3">
+                        {items.map((item) => (
+                            <li key={item.id} className="overflow-hidden rounded-md bg-white px-6 py-4 shadow-sm">
+                                {/* Your content */}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
         </div>
     )
-
 }
