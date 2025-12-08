35b5e4234d27a97c1180935580dc028acb56cdba
Thomas Rizzo <rizzot@oregonstate.edu>
2025-12-08 12:50:09 -0800

    race card styling

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -23,16 +23,16 @@ export default function Calendar() {
 
     return (
         <div className="mx-auto pt-10 max-w-7xl px-4 sm:px-6 lg:px-8 h-screen">
-            Race Schedule
+            <h1 className="text-center text-3xl font-bold mb-6">Race Schedule</h1>
                 <div className="mx-auto max-w-3xl mt-6">
                     <ul role="list" className="space-y-3">
                         {races.map((race) => (
                             <li key={race.id} className="overflow-hidden rounded-md bg-white px-6 py-4 shadow-sm">
-                                <h1>{race.racename}</h1>
-                                <h2>Date: {race.racedate}</h2>
-                                <h2>Time: {race.racetime}</h2>
-                                <h3>Location: {race.water}</h3>
-                                <p>Class: {race.boatclass}</p>
+                                <h1 className="font-bold text-xl mb-3">{race.racename}</h1>
+                                <h2 className="text-lg mb-1">Date: {race.racedate}</h2>
+                                <h2 className="text-lg mb-1">Time: {race.racetime}</h2>
+                                <h3 className="text-base mb-1">Location: {race.water}</h3>
+                                <p className="text-sm text-gray-600">Class: {race.boatclass}</p>
                             </li>
                         ))}
                     </ul>
